e10c6e1471e041cce1faa728e66d8c64e038f787
Jorgen Tuck <jamesjtuck@gmail.com>
2021-04-07 18:40:42 -0600

    added under construction message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container, Row } from 'react-bootstrap';
+// import NavBar from './components/NavBar';
+import Home from './pages/Home';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
+// import Error from './pages/Error';
+// import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* <NavBar /> */}
+        <Container fluid>
+          <Row>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              {/* <Route exact path='/about' component={About} /> */}
+              {/* <Route exact path='/contact' component={Contact} /> */}
+              {/* <Route component={Error} /> */}
+            </Switch>
+          </Row>
+        </Container>
+        {/* <Footer /> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DATA } from '../utils/data'
+
+export default function Home() {
+
+    
+
+
+
+    return (
+        <div>
+            Under construction, please come back later
+        </div>
+    )
+}

0ffbe8a2e83950e05ae8afa9fb0be506505d468e
Jorgen Tuck <jamesjtuck@gmail.com>
2021-05-25 20:23:25 -0600

    updated footer to heretic coding

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className='footer'>
         <div className='container-fluid'>
-          <span className='text-muted'> &copy; Arrowhead Towing Service 2021 - Built by <a href='https://www.jorgentuck.com/portfolio' target='_blank' rel="noreferrer">Jorgen Tuck</a></span>
+          <span className='text-muted'> &copy; Arrowhead Towing Service 2021 - Built by <a href='https://www.hereticcoding.com/portfolio' target='_blank' rel="noreferrer">Heretic Coding</a></span>
         </div>
       </footer>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 
 export default function Home() {
 
-    
+
 
 
 

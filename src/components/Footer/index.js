98ce306687890bac62cbaf2280ea98c2d566814d
Jorgen Tuck <jamesjtuck@gmail.com>
2021-05-25 20:25:35 -0600

    updated footer with All rights reserved

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className='footer'>
         <div className='container-fluid'>
-          <span className='text-muted'> &copy; Arrowhead Towing Service 2021 - Built by <a href='https://www.hereticcoding.com/portfolio' target='_blank' rel="noreferrer">Heretic Coding</a></span>
+          <span className='text-muted'> &copy; Arrowhead Towing Service 2021. All rights reserved - Built by <a href='https://www.hereticcoding.com/portfolio' target='_blank' rel="noreferrer">Heretic Coding</a></span>
         </div>
       </footer>
     )

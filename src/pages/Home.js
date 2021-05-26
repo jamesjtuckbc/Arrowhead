f03f6766942d21822855079a512eca2107553f68
Jorgen Tuck <jamesjtuck@gmail.com>
2021-05-25 20:27:34 -0600

    removed extra spaces

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,6 @@ import { Container } from 'react-bootstrap';
 
 export default function Home() {
 
-
-
-
-
     return (
         <Container fluid className='text-center'>
             Under construction, please come back later
